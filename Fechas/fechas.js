fbc336c03eaa8c807e22330b3ca0dd1ef856ea80
Rafael Cisneros Gómez <rafael.cisneros.gomez@gmail.com>
2016-12-12 01:57:47 -0600

    Utilizando clases y fechas
    Ejercicio básico del uso de fechas y Clases.

---
+++ Fechas/fechas.js
@@ -0,0 +1,36 @@
+/*
+   McB0y.
+   Ejemplos de como se utiliza el objeto Date() para generar fechas.
+
+*/
+
+
+//Utilizando herencia multiple y clases podemos implementar lo sig.
+   class fecha extends Date {
+      constructor() {
+         super();//todos los métodos y atributos de Date();
+      }
+
+      sumarDias(dias){
+      this.setDate( this.getDate()  + dias);
+      return this;
+      }
+
+      sumarAnios(anios){
+      this.setFullYear( this.getFullYear()  + anios );
+      return this;
+      }
+
+      detalleFecha(){
+         console.log("Día : " + this.getDate() + ", Mes : " + this.getMonth() + ", Año : " + this.getFullYear());
+      }
+   };
+
+
+var fecha = new Date(2016, 9, 20);
+console.log(fecha1);
+console.log(fecha1.sumarAnios(-20));
+console.log(fecha1.sumarDias(-20));
+console.log(fecha1.detalleFecha());
+
+//Se pueden generar más petodos en la clase para el manejo de fechas
