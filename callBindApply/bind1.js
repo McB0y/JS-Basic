7483f92dcf6460ebe05940202fcd34db0b021d4b
Rafael Cisneros Gómez <rafael.cisneros.gomez@gmail.com>
2016-12-14 08:32:49 -0600

    Más sobre funciones prestadas.

---
+++ callBindApply/bind1.js
@@ -0,0 +1,111 @@
+/*
+
+McB0y
+
+   Ejercicios utilizando "BIND"
+
+*/
+
+
+var desktopComputer = {
+   monitor : "OLED",
+   teclado : "mecánico",
+   refrijeracion : "cooler liquido",
+   procesador : "i5 Intel",
+   tarjetaDeRed : "Alto desempeño",
+   almacenamiento : "SSD 1TB",
+   encender : function() {
+      console.log("Encendiendo el monitor " + this.monitor);
+   },
+   conectar : function() {
+      console.log(this.tarjetaDeRed + " para la conexión");
+   }
+};
+
+var laptop = {
+   monitor : "LCD",
+   teclado : "reticular",
+   refrijeracion : "cooler master",
+   procesador : "AMD A10",
+   almacenamiento : "SSD 1TB",
+   encender : function() {
+      console.log("Encendiendo...");
+   }
+};
+
+
+var laptopGaming = {
+   monitor : "OLED 17' ",
+   teclado : "mecánico",
+   refrijeracion : "refrijeración liquida",
+   procesador : "i7 Intel",
+   tarjetaDeRed : "Alto desempeño",
+   almacenamiento : "SSD 1TB",
+   overclocking : function(){
+      console.log("Acelerando nuestro proesador al máximo!");
+   },
+   modoConsola : function() {
+      console.log("PS4, XBOX ONE, NINTENDO, EMULS," +
+                  " sin importar que sea correra mil veces mejor aquí... xD ");
+   }
+
+};
+
+
+var raspBerryPi = {
+   monitor : "sin monitor ",
+   teclado : "sin teclado",
+   procesador : "ARM",
+   conexionDeRed : "J45 ETHERNET",
+   almacenamiento : "micro SD expandible...",
+   gpios : 40,
+   conectarTeclado : function(tipoTeclado) {
+      console.log("Conectando nuestro teclado de tipo : " + tipoTeclado);
+   },
+   conectarPantalla : function(tipoPantalla) {
+      console.log("Conectando un@, " + tipoPantalla);
+   },
+   desarrolo : function(componentes, aplicacionIoT) {//componentes es un arreglo
+      console.log("Desarrollando proyecto de IoT...");
+      console.log("Material : " + componentes + " " + "aplicada en : " + aplicacionIoT);
+   }
+};
+
+
+//1.-raspBerryPi pide prestado el metodo encender() de desktopComputer
+
+
+function encenderPi(){
+   return this.encender();//queremos que apunte a quien tiene el método.
+}
+
+var accion = encenderPi.bind(desktopComputer);//Pasamos al parametro el objeto al que apunta this
+//podria ser tambien así al ejecutar: encenderPi.bind(desktopComputer)();
+
+accion();
+
+
+
+//2.- laptop pide prestado el metodo overclocking() de la laptopGaming
+
+function acelerando() {
+console.log("Habra overclocking, bajo su propio riesgo : ");
+return this.overclocking();
+}
+
+
+var accion = acelerando.bind(laptopGaming);//Pasamos al parametro el objeto al que apunta this
+//podria ser tambien así al ejecutar: encenderPi.bind(desktopComputer)();
+
+accion();
+
+
+//3.-Pidiendo prestadas propiedades.
+
+function cambioCPU() {
+   console.log("Antes tenia un procesador de gama media ahora...");
+   return this.procesador;
+}
+
+var swit = cambioCPU.bind(laptopGaming);
+swit();
--- callBindApply/call.js
+++ callBindApply/call.js
@@ -9,12 +9,54 @@
 
 */
 
+var carro = {
+   marca : "Mazda",
+   color : "Rojo",
+   imprimir : function(){
+      var salida = this.marca + "-" + this.color;
+      return salida;
+   }
+}
+
+
+var carro2 = {
+   color : "Negro",
+   marca : "Toyota"
+}
+
+
+
+
+
+
+
+ function logCarro(arg1, arg2) {
+   console.log("Carro :" + this.imprimir());//apunta al obj global
+   //por el momento el this apunta hacia el objeto global.
+   console.log("nuestros argumentos serian : " + arg1 + " " + arg2);
+   console.log("===============================================");
+}
+
+
+
+logCarro();//error, redireccionamos el apuntador hacia el objeto:
+
+
+
+
+var logModeloCarro = logCarro;
+
+//esto se coloca en el this invocando la funcion bind()
+//podemos acceder ahora al  contexto del objeto carro.
+logModeloCarro();
+
+logModeloCarro("abc", "xyz");
 
 //una manera más fácil de hacerlo seria utilizando la función call(apuntador al this, ar1, arg2, arg3);
 
-logModeloCarro.call( carro , "Hola", "Mundo");// Log modelo carro hace ref a logCarro y es por eso que necesita estos datos.
+logModeloCarro.call( carro , "Hola", "Mundo");// Log modelo carro hace ref. a logCarro y es por eso que necesita estos datos.
 logModeloCarro.apply( carro , ["Gente", "Del futuro"]);//apply espera la referencia del this y un arreglo de argumentos.
-//"Muy útil si no se sabe la cantidad de parametros"
+//"Muy útil si no se sabe la cantidad de parámetros"
 
 
 
