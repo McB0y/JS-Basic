f5d5a7471e8fa57d00ce3c429d6530cc78b8fdd7
Rafael Cisneros Gómez <rafael.cisneros.gomez@gmail.com>
2016-12-20 01:13:11 -0600

    Hoisting
    Algunos casos donde se da el comportamiento de elevación en JS.

---
+++ Funciones/funciones2.js
@@ -0,0 +1,8 @@
+/*
+
+Hoisting: Un comportamiento que existe en Javascript donde todas las variables
+se elevan a la primera linea del documento, por medio de la consola podemos ver que
+se regresa la sentencia "undefined" hasta que la variable es llamada,
+o la función es ejecutada, a continuación un par de casos.
+
+*/
