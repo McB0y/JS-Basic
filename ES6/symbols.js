781904f4421f880d275f148f231d665796ef32dc
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-05 14:35:13 -0600

    ES6
    Symbols y su uso

---
+++ ES6/symbols.js
@@ -0,0 +1,44 @@
+/*
+         Simbolos.
+
+Los simbolos son una tipo de dato nativo comolo son las cadenas,
+y nos permiten agregar proiedadesa los objetos donde el simbolo
+seria el key.
+Los simbolos no son iterables, lo que quiere decir que si iteramos no vemremos
+los simbolos iterarse como si fueran el key de un objeto
+El constructor Symbol no debe ser usado con el operador new.
+Tampoco debe ser extendido mediante clases.
+*/
+
+//Symbol([description])
+let symbol = Symbol('debug');
+
+
+   console.log(symbol);//devuelve un objeto symbol
+ // console.log(typeof symbol);  //symbol
+ //console.log(symbol.toString); especialmente útil si se quiere saber de que se esta hablando
+
+
+ //Los simbolos son unicos ya que por en el fondo son identificados por un id.
+
+let symbol = Symbol('debug');
+let symbol2 = Symbol('debug');
+
+console.log(symbol == symbol2);//FALSE!!
+
+
+let Persona = {
+   name : "Rose",
+   [symbol] : 27
+}
+
+console.log(Persona);// {name : "Rose"}
+console.log(Persona[symbol]);//27
+
+//Si se quiere tener 2 simbolos que compartan el mismo identificador es posible
+
+let chica = Symbol.for('edad');
+let chica1 = Symbol.for('edad');
+
+console.log(chica1 == chica);//true
+console.log(chica1 === chica);//true
