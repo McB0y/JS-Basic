2e057d4005e246a0fb1b9b56f0db6fef05064f77
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-05 09:22:26 -0600

    ES6
    Métodos static

---
+++ ES6/Clases_ES6/static.js
@@ -0,0 +1,39 @@
+/*export*/ class Helper {//agregando export podemos llevarnos el script a otro script
+   static logTwice(){
+      console.log("Me llamare 1");
+      console.log("NO! dos veces...");
+   }
+}
+
+//En otros leguajes como Java se puede hacer esto también ya que
+//los métodos de clase son accesibles directamente con el nombre de la clase.
+Helper.logTwice();
+
+
+
+/*
+Podemos definir getters y setters en las clases,
+teniendo atributos privados
+*/
+
+
+class Persona {
+   constructor(name) {
+      this._name = name;//el '_' significa que es privado
+   }
+
+   get name(){
+      return this._name.toUpperCase();
+   }
+
+   set name(value){
+      if(value.lenght>2){
+         this.name = value;
+      }
+      console.log("rejected!");
+   }
+}
+
+let person = new Person("Jane");
+
+console.log("person");
