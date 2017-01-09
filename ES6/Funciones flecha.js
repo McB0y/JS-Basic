9e71910b9161dbe60c0aedd85cb0d21c765aba21
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-09 16:30:35 -0600

    Funciones Arrow
    Más sobre las funciones flecha

--- ES6/Funciones flecha.js
+++ ES6/Funciones flecha.js
@@ -1,7 +1,14 @@
 /*
 
-Uso de funciones Fat Arrow (Flecha)
+Uso de funciones Fat Arrow (Flecha), tienen ciertas peculiaridades con respecto a la especificación pasada ES5,
+ya que no se usa de la misma forma las siguienete palabras: "this, super, arguments, new.target" esto debido 
+a que siempre se apunta a la función padre no flecha, por tanto no hay;
 
+-llamadas con new invalidas ya que no pueden ser utilizadas como constructor
+-no prototipos (ya que no hay necesidad)
+-no cambia el contexto debido a un lexical this
+-no hay bind con el objeto 'arguments'
+-no puede haber nombres duplicados en los parámetros
 */
 
 "use strict";
@@ -67,3 +74,51 @@ function fn() {
 var fn = () => console.log(this);// De la misma forma regresa el obj. global
 //Las funciones Flecha siempre mantendran su contexto, asi que no se necesitan
 //call, bind o apply esa es la principal diferencia
+
+
+//LO siguientes ejemplos fueron sacados directamente de la documentación de Mozilla:
+
+var adder = {
+  base : 1,
+  add : function(a) {
+    var f = function(v) {
+      console.log(v+this.base);
+    }
+    return f(a);
+  },
+  addThruCall: function(a) {
+    var f = v => v + this.base;
+    var b = {
+      base : 2
+    };
+    return f.call(b, a);
+  }
+};
+
+console.log(adder.add(1));         // This would log to 2
+console.log(adder.addThruCall(1)); // This would log to 2 still
+
+
+
+
+
+var miAr = function(a,b,c){console.log(arguments)};
+
+var miAr = (a,b,c) => console.log(arguments);//No existe el obj.arguments
+
+
+
+
+
+
+
+'use strict';
+var obj = {
+  i: 10,
+  b: () => console.log(this.i, this),
+  c: function() {
+    console.log( this.i, this)
+  }
+}
+obj.b(); // prints undefined, Window
+obj.c(); // prints 10, Object {...}
