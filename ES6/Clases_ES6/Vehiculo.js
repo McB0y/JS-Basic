3ee551fca718a9132e021f796cc4499dcaff4408
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-13 14:35:22 -0600

    ES6
    Depurando

--- ES6/Clases_ES6/Vehiculo.js
+++
@@ -1,9 +0,0 @@
-/*
-   McB0y.
-   Uso de clases con la especificación ES6, cabe aclarar que por debajo es una función prototipal.
-   "Factory Pattern".
-
-   Puedes consultar la siguiente bibliografía para más ejemplos:
-   Título: Aprendiendo Javascript
-   Autor : Carlos Azaustre.
-*/
