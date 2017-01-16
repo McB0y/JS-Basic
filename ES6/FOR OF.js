618259077b88cedae1fd55e3a5b5d882f1ee46bc
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-16 15:35:38 -0600

    ES6
    Rotulando ciclos for of

--- ES6/FOR OF.js
+++ ES6/FOR OF.js
@@ -26,3 +26,36 @@ for(let i in gradoAcademico){
 for(let nivel of gradoAcademico){
   console.log(nivel);
 }
+
+
+/*Rotular Ciclos con for of es muy fácil tomando de ejemplo lo anterior:*/
+
+var oso = ["Pardo", "Polar", "Grisley", "Mielero", "Panda", "Indú", "Hormiguero"];
+
+
+rotulado: for(i of oso){
+   if(i !== "Polar")
+      {
+         rotulado1: for(j of oso)
+            if(j === "Mielero")
+            {
+               console.log("Saliendo del segundo ciclo!")
+               break rotulado1;
+            }
+            else
+               console.log(j);
+      }
+   else{
+      console.log("Saliendo del primer ciclo!");
+      break rotulado;
+   }
+}
+
+/*
+La salida :
+Pardo
+Polar
+Grisley
+Saliendo del segundo ciclo!
+Saliendo del primer ciclo!
+*/
