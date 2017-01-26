82994a3475fd0e00689a6d2273d175032d0f2f3e
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-26 12:19:07 -0600

    ES6
    Reflect API: un herramienta que fue integrada en ES6 para hacer
    metaprogramación por medio de Reflect object, teniendo antes acceso por
    medio de Object object.

---
+++ ES6/Reflect API/ReflectObject.js
@@ -0,0 +1,10 @@
+/*
+McB0y : El Reflect API que se basa en metaprogramación.
+        Lo que significa que nos permite manipular el comportamiento
+        que en el que pordefecto esta construido nuestro lenguaje,
+        muchas de sus funcionalidades estaban disponibles anteriormente
+        en el Object Object ahora estandolo en el Reflect Object.
+        Su principal meta del API fue tener disponibles todos estos
+        objetos y metodos en un solo lugar para poder mejorar los proyectos
+        JavaScrip.
+*/
