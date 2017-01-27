fa3f26db0695b5f37f88b67c43d202bc8df4a923
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-27 13:19:15 -0600

    ES6
    Creando el primer proxy.

--- ES6/Proxy API/proxy.js
+++ ES6/Proxy API/proxy.js
@@ -20,3 +20,28 @@ var boy ={
 //El constructor espera 2 argumentos
 //el primeros es el "target", el segundo el "handler"
 //este segundo es el que contiene toda la lógica de nuestro proxy.
+
+
+
+// el handler puede crear todas las trampas que quieras,
+//pues el el el PROXY lo que recibe el get es el objetivo
+//"el objeto", y el segundo es la propiedad a la que accesar.
+
+
+
+
+var handler = {
+   get : function(target, name) {
+      return name in target ?  target[name] : "No existe";
+   }
+
+   set : function(target, property, value) {//exacactamente igual que el Reflect API
+
+   }
+}
+
+var proxy = new Proxy(boy, handler);
+
+console.log(proxy.name);
+console.log(proxy.age);
+console.log(proxy.surname);
