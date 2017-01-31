34cd93a39d35794d2ade6708079a10fcf25ebde1
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-31 14:20:17 -0600

    ES6
    Recuperando la info de un array a partir de un símbolo compartido o no.

--- ES6/Symbols/keyFor.js
+++ ES6/Symbols/keyFor.js
@@ -65,3 +65,58 @@ Symbol.keyFor(simbolo);//RECUPERAMOS NUESTRA OBJETO-CADENA.
 // NOTE: La salida:
 "{"nombre":"Cuenta Maestra","iva":"no aplica","CAT":7.8,"tasaMoratoriaAnual":60.61,"tasaMoratoriaMensual":5.05}"
 */
+
+
+
+
+/*
+Con respecto a los arrays NO IMPORTA si lo convertimos a cadena antes
+ya que el método .keyFor() nos regresa esa cadena que necesitamos.
+*/
+
+
+var datosCliente = ["Pancho Linez Tador",
+                     028623278,
+                    "Está es sólo una prueba de los domicilios.",
+                    "Maestra",
+                     25000];
+
+
+//Símbolo compatido del que se puede recuperar su identificador.
+var simbolo2 = Symbol.for(datosCliente);
+
+
+Symbol.keyFor(simbolo2);//NOS REGRESA UNA CADENA CON LA INF EL ARRAY.
+
+// NOTE: la salida
+//"Pancho Linez Tador,28623278,Está es sólo una prueba de los domicilios.,Maestra,25000"
+
+
+var arrCadena = Symbol.keyFor(simbolo2);
+
+var arrFinal = arrCadena.split(",");//tenemos un array de strings.
+
+// NOTE: SUPER IMPORTANTE! si hay valores numericos en la cadena que se
+// NOTE: genera con el método split(",") nuestro array sera "UNA CADENA"
+// NOTE: por tanto si se desea trabajar con los valores numericos
+// NOTE: podemos ocupar metodos o expresiones reg. y recuperar tal cual el array.
+
+
+/*
+SINO SE QUISIERA TODO ESTE ROLLO PORQUÉ SE QUIERE UN SÍMBOLO ÚNICO
+PODEMOS HACER LO SIGUIENTE :
+*/
+
+var f = Symbol(datosCliente);
+
+var simboloCadena = f.toString();//Nos regresa todo el símbolo como una cadena.
+
+var inicio = simboloCadena.indexOf("(") + 1;
+var final = simboloCadena.indexOf(")");
+
+
+var resul = simboloCadena.substring(inicio, final);
+var array = resul.split(",");
+
+// NOTE: Y lo mismo se usarian otros métodos si lo que se quiere
+// NOTE: es trabajar con los valores del array que son completamente cadenas
