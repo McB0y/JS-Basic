e9c8adf057389eaeadea84a3769998962ee78c9f
McB0y <rafael.cisneros.gomez@gmail.com>
2017-01-31 14:18:59 -0600

    ES6
    Recuperando un Obj.

--- ES6/Symbols/keyFor.js
+++ ES6/Symbols/keyFor.js
@@ -24,3 +24,44 @@ Symbol.keyFor(Symbol.iterator) // undefined
 
 // NOTE: solo funciona con claves compartidas y
 //es para obtener el valor de las claves
+
+
+
+// NOTE: esto podria ser útil si queremos pasar
+//un objeto o un arreglo como identificador, y
+//quisieramos recuperar de nuevo la estructura.
+
+
+
+//1.- El objeto que sera identificador desde ahorita.
+var desCuenta  = {
+   nombre : "Cuenta Maestra",
+   iva : "no aplica",
+   CAT : 7.8,
+   tasaMoratoriaAnual : 60.61,
+   tasaMoratoriaMensual : 5.05
+}
+
+//2.- Si es un objeto, debemos convertirlo a cadena
+
+
+
+var aCadena = JSON.stringify(desCuenta);
+
+
+var simbolo = Symbol.for(aCadena);
+
+
+// NOTE:Symbol("obj") --> de esta manera no se podra recuperar
+// NOTE: debe ser compartido .for(); de lo contrario regresa "undefined".
+// NOTE: no importa si es arreglo u obj
+
+
+Symbol.keyFor(simbolo);//RECUPERAMOS NUESTRA OBJETO-CADENA.
+
+
+
+/*
+// NOTE: La salida:
+"{"nombre":"Cuenta Maestra","iva":"no aplica","CAT":7.8,"tasaMoratoriaAnual":60.61,"tasaMoratoriaMensual":5.05}"
+*/
